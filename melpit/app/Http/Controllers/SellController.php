3baa82fddb2b5bea6f4b4bfb1193f400b41944a2
mzw86020424 <mzw86020424@gmail.com>
2021-04-14 22:19:01 +0900

    商品出品機能を実装

--- melpit/app/Http/Controllers/SellController.php
+++ melpit/app/Http/Controllers/SellController.php
@@ -2,9 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\SellRequest;
+use App\Models\Item;
 use App\Models\ItemCondition;
 use App\Models\PrimaryCategory;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class SellController extends Controller
 {
@@ -22,5 +25,22 @@ class SellController extends Controller
             ->with('categories', $categories)
             ->with('conditions', $conditions);
 
+    }
+
+    public function sellItem(SellRequest $request)
+    {
+        $user = Auth::user();
+
+        $item                           = new Item();
+        $item->seller_id                = $user->id;
+        $item->name                     = $request->input('name');
+        $item->description              = $request->input('description');
+        $item->secondary_category_id    = $request->input('category');
+        $item->item_condition_id        = $request->input('condition');
+        $item->price                    = $request->input('price');
+        $item->state                    = Item::STATE_SELLING;
+        $item->save();
+
+        return redirect()->back()->with('status', '商品を出品しました。');
     }
 }
--- melpit/routes/web.php
+++ melpit/routes/web.php
@@ -24,6 +24,7 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::middleware('auth')
     ->group(function () {
         Route::get('sell', 'SellController@showSellForm')->name('sell');
+        Route::post('sell', 'sellController@sellItem')->name('sell');
     });
 
 Route::prefix('mypage')
