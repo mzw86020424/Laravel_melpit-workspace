9032626336677cc0466301cfd23f65471f0514d4
mzw86020424 <mzw86020424@gmail.com>
2021-04-18 16:03:31 +0900

    商品購入画面を作成

--- melpit/app/Http/Controllers/ItemsController.php
+++ melpit/app/Http/Controllers/ItemsController.php
@@ -20,7 +20,7 @@ class ItemsController extends Controller
                 $query->whereHas('secondaryCategory', function ($query) use ($categoryID) {
                     $query->where('primary_category_id', $categoryID);
                 });
-            } else if ($categoryType === 'secondary') {
+            } elseif ($categoryType === 'secondary') {
                 $query->where('secondary_category_id', $categoryID);
             }
         }
@@ -28,13 +28,13 @@ class ItemsController extends Controller
         // キーワードで絞り込み
         if ($request->filled('keyword')) {
             $keyword = '%'.$this->escape($request->input('keyword')).'%';
-            $query->where(function($query)use($keyword){
-                $query->where('name','LIKE',$keyword);
-                $query->orWhere('description','LIKE',$keyword);
+            $query->where(function ($query) use ($keyword) {
+                $query->where('name', 'LIKE', $keyword);
+                $query->orWhere('description', 'LIKE', $keyword);
             });
         }
 
-        $items = $query->orderByRaw( "FIELD(state, '" . Item::STATE_SELLING . "', '" . Item::STATE_BOUGHT . "')" )
+        $items = $query->orderByRaw("FIELD(state, '" . Item::STATE_SELLING . "', '" . Item::STATE_BOUGHT . "')")
             ->orderBy('id', 'DESC')
             ->paginate(52);
 
@@ -54,6 +54,16 @@ class ItemsController extends Controller
     public function showItemDetail(Item $item)
     {
         return view('items.item_detail')
+            ->with('item', $item);
+    }
+
+    public function showBuyItemForm(Item $item)
+    {
+        if (!$item->isStateSelling) {
+            abort(404);
+        }
+
+        return view('items.item_buy_form')
             ->with('item', $item);
     }
 }
